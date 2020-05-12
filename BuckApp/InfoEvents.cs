c72472928842b6e3edf302b4b87bfcc5a9a7f622
alex gonzalez <alex_tamudin@hotmail.com>
2020-05-12 23:44:12 +0200

    ejemplo

--- BuckApp/InfoEvents.cs
+++ BuckApp/InfoEvents.cs
@@ -14,7 +14,7 @@ namespace BuckApp
     {
         String name, description, localidad, fecha, type;
         int n_participantes, n_maxParticipantes, id_user, userID;
-
+        //Hola esto es una prueba
         public InfoEvents(String name, String description, String localidad, String fecha, 
             int n_participantes, int n_maxParticipantes, String type, int id_user, int userID)
         {
